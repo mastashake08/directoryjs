2bdba7d2cd400aff0b2acce0fb5a84ba09ff6cba
Hacker God <jyrone.parker@gmail.com>
2023-10-13 01:01:52 -0400

    v1.4.0

--- ShakeFile.js
+++ ShakeFile.js
@@ -1,112 +1,15 @@
 class ShakeFile {
-    constructor () {
-        this.file = null
-        this.obj = null
+    constructor ({file = null, obj = null, showDom = false}) {
+        this.file = file
+        this.obj = obj
         this.mainDiv = document.createElement('div')
         this.mainDiv.id = 'main-div'
         this.encodedData = null
         this.decodedData = ''
-
+        this.showDom = showDom
         document.body.appendChild(this.mainDiv)
-    }
-    getFilenameAndExtension(pathfilename){
-
-        var filenameextension = pathfilename.replace(/^.*[\\\/]/, '');
-        var filename = filenameextension.substring(0, filenameextension.lastIndexOf('.'));
-        var ext = filenameextension.split('.').pop();
-        
-        return [filename, ext];
-      
-    }
-
-    async saveFile() {
-        try {
-            const [filename] = this.getFilenameAndExtension(this.file.name);
-
-            // create a new handle
-           const newHandle = await window.showSaveFilePicker({
-            suggestedName: Date.now() + '-' + filename + '.txt'
-           });
-         
-           // create a FileSystemWritableFileStream to write to
-           const writableStream = await newHandle.createWritable();
-         
-           // write our file
-           let my_uint8_array = Uint8Array.from(this.obj.value, c => c.charCodeAt(0)); 
-           await writableStream.write(new Blob([my_uint8_array], {type: 'text/plain'}));
-         
-           // close the file and write the contents to disk.
-           await writableStream.close();
-           alert('File SAVED!')
-           return true   
-           } catch (error) {
-               alert(error.message)
-               return false
-           }
-    }
-    async saveBinaryFile() {
-        try {
-            const [filename, ext] = this.getFilenameAndExtension(this.file.name);
-            // create a new handle
-           const newHandle = await window.showSaveFilePicker({
-            suggestedName: Date.now() + filename + '.' + ext
-           });
-           this.encodedData = Uint8Array.from([...this.obj.value].map(ch => ch.charCodeAt())).buffer;
-           // create a FileSystemWritableFileStream to write to
-           const writableStream = await newHandle.createWritable();
-         
-           // write our file
-            await writableStream.write(new Blob([this.encodedData]), {type: this.file.kind});
-         
-           // close the file and write the contents to disk.
-           await writableStream.close();
-           alert('File SAVED!')
-           return true   
-           } catch (error) {
-               alert(error.message)
-               return false
-           }
-    }
-    async getData () {
-        const buffer = await this.file.arrayBuffer()
-        const contents = {
-            name: this.file.name,
-            kind: this.file.kind,
-            text: await this.file.text(),
-            stream: await this.file.stream(),
-            arrayBuffer: buffer,
-            dataView: new DataView(buffer),
-            int8Array: new Int8Array(buffer),
-            uint8Array: new Uint8Array(buffer),
-            size: this.file.size,
-            lastModified: this.file.lastModified,
-            lastModifiedDate: this.file.lastModifiedDate,
-            type: this.file.type,
-            domEl: this.addToDom()
-
-
-        }
-        this.data = contents
-        return contents
-    }
-    async setFile(file) {
-        this.file = file
-    }
-    setProgress(reader) {
-        const progress = document.createElement('progress')
-        progress.id = Date.now().toString()
-        const label = document.createElement('label')
-        label.innerText = 'Progress:'
-        label.setAttribute('for', progress.id) 
-        label.appendChild(progress)
-        this.mainDiv.appendChild(label)
-        progress.appendChild(document.createElement('br'))
-        reader.onprogress = (evt) => {
-            progress.max = evt.total
-            progress.value = evt.loaded
-        }
-        reader.onloadend = () => {
-            this.fadeOut(label)
+        if(this.showDom) {
+            this.addToDom()
         }
     }
 
@@ -151,22 +54,124 @@ class ShakeFile {
         
     }
 
-    setVideo(file, reader) {
-        reader.readAsDataURL(file)
-        reader.onload = () =>{
-                        const dataUrl = reader.result
-                        this.obj = document.createElement('video')
-                        this.obj.style.resize = 'both'
-                        this.obj.setAttribute("controls","controls") 
-                        this.obj.setAttribute("x-webkit-airplay","allow")
-                        this.obj.setAttribute("cast","allow")
-                        this.obj.disableRemotePlayback = false
-                        this.obj.style.width = '250px'
-                        this.obj.style.height = '250px'
-                        this.obj.style.marginBottom = '5px'
-                        this.obj.src = dataUrl
-                        this.mainDiv.appendChild(this.obj)
+    fadeOut(el) {
+        var opacity = 1; // Initial opacity
+        var interval = setInterval(function() {
+           if (opacity > 0) {
+              opacity -= 0.1;
+              el.style.opacity = opacity;
+           } else {
+              clearInterval(interval); // Stop the interval when opacity reaches 0
+              el.style.display = 'none'; // Hide the element
+              el.remove()
+           }
+        }, 100);
+    }
+
+    async getData () {
+        const buffer = await this.file.arrayBuffer()
+        const contents = {
+            name: this.file.name,
+            kind: this.file.kind,
+            text: await this.file.text(),
+            stream: await this.file.stream(),
+            arrayBuffer: buffer,
+            dataView: new DataView(buffer),
+            int8Array: new Int8Array(buffer),
+            uint8Array: new Uint8Array(buffer),
+            size: this.file.size,
+            lastModified: this.file.lastModified,
+            lastModifiedDate: this.file.lastModifiedDate,
+            type: this.file.type,
+            domEl: this.showDom ? this.addToDom() : {}
+
+
         }
+        this.data = contents
+        return contents
+    }
+
+    getFilenameAndExtension(pathfilename){
+
+        var filenameextension = pathfilename.replace(/^.*[\\\/]/, '');
+        var filename = filenameextension.substring(0, filenameextension.lastIndexOf('.'));
+        var ext = filenameextension.split('.').pop();
+        
+        return [filename, ext];
+      
+    }
+    getReader(options = {}) {
+        return this.getStream().getReader(options)
+    }
+
+    getStream() {
+        return this.file.stream()
+    }
+
+    pipeThrough(destination, options = {
+        preventClose: false,
+        preventAbort: false,
+        preventCancel: false,
+        signal: {}
+    }) {
+        return this.getStream().pipeThrough(destination, options)
+    }
+
+    pipeTo(destination, options = {
+        preventClose: false,
+        preventAbort: false,
+        preventCancel: false,
+        signal: {}
+    }) {
+        return this.getStream().pipeTo(destination, options)
+    }
+    async saveBinaryFile() {
+        try {
+            const [filename, ext] = this.getFilenameAndExtension(this.file.name);
+            // create a new handle
+           const newHandle = await window.showSaveFilePicker({
+            suggestedName: Date.now() + filename + '.' + ext
+           });
+           this.encodedData = Uint8Array.from([...this.obj.value].map(ch => ch.charCodeAt())).buffer;
+           // create a FileSystemWritableFileStream to write to
+           const writableStream = await newHandle.createWritable();
+         
+           // write our file
+            await writableStream.write(new Blob([this.encodedData]), {type: this.file.kind});
+         
+           // close the file and write the contents to disk.
+           await writableStream.close();
+           alert('File SAVED!')
+           return true   
+           } catch (error) {
+               alert(error.message)
+               return false
+           }
+    }
+    async saveFile() {
+        try {
+            const [filename] = this.getFilenameAndExtension(this.file.name);
+
+            // create a new handle
+           const newHandle = await window.showSaveFilePicker({
+            suggestedName: Date.now() + '-' + filename + '.txt'
+           });
+         
+           // create a FileSystemWritableFileStream to write to
+           const writableStream = await newHandle.createWritable();
+         
+           // write our file
+           let my_uint8_array = Uint8Array.from(this.obj.value, c => c.charCodeAt(0)); 
+           await writableStream.write(new Blob([my_uint8_array], {type: 'text/plain'}));
+         
+           // close the file and write the contents to disk.
+           await writableStream.close();
+           alert('File SAVED!')
+           return true   
+           } catch (error) {
+               alert(error.message)
+               return false
+           }
     }
 
     setAudio(file, reader) {
@@ -185,24 +190,7 @@ class ShakeFile {
                         this.mainDiv.appendChild(this.obj)
         }
     }
-    async setText(file, reader) {
-        const dataUrl = await file.text()
     
-        this.obj = document.createElement('textarea')
-        this.obj.style.resize = 'both'
-        this.obj.style.width = '100%'
-        this.obj.style.height = '300px'
-        this.obj.value = dataUrl
-        const save = document.createElement('button')
-        save.innerHTML = 'Save File'
-        save.onclick = () => {
-         this.saveBinaryFile()
-        }    
-        this.mainDiv.appendChild(this.obj)
-
-        this.obj.after(save)
-    }
-
     async setBinary(file, reader) {
         const data = await file.arrayBuffer()
         const uint = new Uint8Array(data)
@@ -225,35 +213,84 @@ class ShakeFile {
         this.obj.after(save)  
     }
 
+    setFile(file) {
+        this.file = file
+    }
+        
     setObject(file, reader) {
-        reader.readAsDataURL(file)
-        reader.onload = async() => {
-                        const dataUrl = reader.result
-                        this.obj = document.createElement('object')
-                        
-                        this.obj.style.width = 'auto'
-                        this.obj.style.height = 'auto'
-                        this.obj.data = dataUrl   //DataURL
-                        this.obj.type = file.type
-                        this.mainDiv.appendChild(this.obj)
-                        
+            reader.readAsDataURL(file)
+            reader.onload = async() => {
+                const dataUrl = reader.result
+                this.obj = document.createElement('object')
+                
+                this.obj.style.width = 'auto'
+                this.obj.style.height = 'auto'
+                this.obj.data = dataUrl   //DataURL
+                this.obj.type = file.type
+                this.mainDiv.appendChild(this.obj)
+                
+            }
+    }
+    
+    setProgress(reader) {
+        const progress = document.createElement('progress')
+        progress.id = Date.now().toString()
+        const label = document.createElement('label')
+        label.innerText = 'Progress:'
+        label.setAttribute('for', progress.id) 
+        label.appendChild(progress)
+        this.mainDiv.appendChild(label)
+        progress.appendChild(document.createElement('br'))
+        reader.onprogress = (evt) => {
+            progress.max = evt.total
+            progress.value = evt.loaded
+        }
+        reader.onloadend = () => {
+            this.fadeOut(label)
         }
     }
 
-    fadeOut(el) {
-        var opacity = 1; // Initial opacity
-        var interval = setInterval(function() {
-           if (opacity > 0) {
-              opacity -= 0.1;
-              el.style.opacity = opacity;
-           } else {
-              clearInterval(interval); // Stop the interval when opacity reaches 0
-              el.style.display = 'none'; // Hide the element
-              el.remove()
-           }
-        }, 100);
-     }
-   
+    async setText(file, reader) {
+            const dataUrl = await file.text()
+        
+            this.obj = document.createElement('textarea')
+            this.obj.style.resize = 'both'
+            this.obj.style.width = '100%'
+            this.obj.style.height = '300px'
+            this.obj.value = dataUrl
+            const save = document.createElement('button')
+            save.innerHTML = 'Save File'
+            save.onclick = () => {
+             this.saveBinaryFile()
+            }    
+            this.mainDiv.appendChild(this.obj)
+    
+            this.obj.after(save)
+    }
+
+    setVideo(file, reader) {
+            reader.readAsDataURL(file)
+            reader.onload = () =>{
+                const dataUrl = reader.result
+                this.obj = document.createElement('video')
+                this.obj.style.resize = 'both'
+                this.obj.setAttribute("controls","controls") 
+                this.obj.setAttribute("x-webkit-airplay","allow")
+                this.obj.setAttribute("cast","allow")
+                this.obj.disableRemotePlayback = false
+                this.obj.style.width = '250px'
+                this.obj.style.height = '250px'
+                this.obj.style.marginBottom = '5px'
+                this.obj.src = dataUrl
+                this.mainDiv.appendChild(this.obj)
+            }
+    }
+
+    tee() {
+        return this.getStream().tee()
+    }
+        
 }
+    
 
 export { ShakeFile }
